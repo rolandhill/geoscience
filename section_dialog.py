e722802859343014b5de4d463d8b4a412a680d39
Roland Hill <roland.hill@mmg.com>
2019-03-25 22:51:53 +0200

    Section manager dialog class.

---
+++ section_dialog.py
@@ -0,0 +1,32 @@
+import os
+
+from PyQt5 import QtCore, uic
+from PyQt5 import QtWidgets
+#from PyQt5 import QtGui
+
+from qgis.core import *
+from qgis.utils import *
+from qgis.gui import *
+
+from .dialogBase import dialogBase
+
+FORM_CLASS, _ = uic.loadUiType(os.path.join(
+    os.path.dirname(__file__), 'section_dialog_base.ui'))
+
+
+class SectionDialog(QtWidgets.QDialog, dialogBase, FORM_CLASS):
+    def __init__(self, manager, parent=None):
+        """Constructor."""
+        super(SectionDialog, self).__init__(parent)
+        
+        # Keep a reference to the DrillManager
+        self.drillManager = manager
+        
+        # Set up the user interface from Designer.
+        # After setupUI you can access any designer object by doing
+        # self.<objectname>, and you can use autoconnect slots - see
+        # http://qt-project.org/doc/qt-4.8/designer-using-a-ui-file.html
+        # #widgets-and-dialogs-with-auto-connect
+        self.setupUi(self)
+    
+
